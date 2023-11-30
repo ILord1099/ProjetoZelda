630e36e77a298520cdfece564c79f80e70fc0691
ILord1099 <Carlinhos.anjos99@gmail.com>
2023-11-29 21:08:19 -0300

    Move Player
    Movimento horizontal e vertical do player, colocando os assets finais.
    configurações de câmera.

---
+++ Projeto Zelda/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    [Header("Config Player")]
+    public float movementSpeed = 3f;
+
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        direction = new Vector3 (horizontal,0f,vertical);
+
+        controller.Move(direction * movementSpeed* Time.deltaTime);
+    }
+}
