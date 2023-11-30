8e30225f75dfd6ce3ee0b00fd3b37ace44ae870a
ILord1099 <Carlinhos.anjos99@gmail.com>
2023-11-29 21:18:51 -0300

    Rotação player
    Implementação da movimentação, colocando a rotação junto com a movimentação

--- Projeto Zelda/Assets/Scripts/PlayerController.cs
+++ Projeto Zelda/Assets/Scripts/PlayerController.cs
@@ -25,7 +25,13 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        direction = new Vector3 (horizontal,0f,vertical);
+        direction = new Vector3 (horizontal,0f,vertical).normalized;
+
+        if(direction.magnitude > 0.1f) 
+        {
+            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0,targetAngle,0);
+        }
 
         controller.Move(direction * movementSpeed* Time.deltaTime);
     }
