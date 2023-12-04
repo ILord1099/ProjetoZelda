11cd9d531ec65e6eec5130f6c0eda49c6fba5abc
ILord1099 <Carlinhos.anjos99@gmail.com>
2023-12-04 20:07:36 -0300

    animação de ataque
    implementação da animaçao de ataque do player

--- Projeto Zelda/Assets/Scripts/PlayerController.cs
+++ Projeto Zelda/Assets/Scripts/PlayerController.cs
@@ -25,8 +25,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Attack");
+        }
+
 
         direction = new Vector3 (horizontal,0f,vertical).normalized;
 
